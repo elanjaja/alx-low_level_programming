d16d695cd431ad785a498db3ce8dc06070744b5f
elanjaja <elanjaja01@gmail.com>
2023-07-13 17:47:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line using the printf function
- *return: 0 (success)
+ *main - entry point
+ *return: always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,\n")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
