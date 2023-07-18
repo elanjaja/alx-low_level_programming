3077d264cb3da16dbdc023c91eaee154d638554e
elanjaja <elanjaja01@gmail.com>
2023-07-17 18:56:25 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints the lowercase alphabet in reverse,
- * followed by a new line 
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
