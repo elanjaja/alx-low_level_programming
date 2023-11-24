b0fa17e9181bee8c5d92de1ade20679816c3b5d4
elanajaja <elanjaja01@gmail.com>
2023-11-23 16:53:22 -0800

    dated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,31 +4,31 @@
 
 /**
  * binary_to_uint - converts binary number to unsigned int
- * @s: string
+ * @b: string
  * Return: Converted number || 0
  */
-unsigned int binary_to_uint(const char *s)
+unsigned int binary_to_uint(const char *b)
 {
 	int slen = 0, i;
 	unsigned int total = 0;
 
-	if (s == NULL)
+	if (b == NULL)
 		return (0);
 
-	while (s[slen] != '\0')
+	while (b[slen] != '\0')
 	{
 		slen++;
 	}
 	slen -= 1;
 	i = 0;
 
-	while (s[i])
+	while (b[i])
 	{
-		if ((s[i] != '0') && (s[i] != '1'))
+		if ((b[i] != '0') && (b[i] != '1'))
 		{
 			return (total);
 		}
-		if (s[i] == '1')
+		if (b[i] == '1')
 		{
 			total += (1 * (1 << slen));
 		}
