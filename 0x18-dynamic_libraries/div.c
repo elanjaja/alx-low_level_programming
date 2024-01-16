ebe9bfc483be6c9c76ea9ff8348049b7517e6b39
elanajaja <elanjaja01@gmail.com>
2024-01-15 16:57:31 -0800

    finally

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * return: divide result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - substract two integers
+ * @a: first int
+ * @b: second int
+ * return: subtract result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
