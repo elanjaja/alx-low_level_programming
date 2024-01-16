4eba9575e2a97ed37b76b1cd9798ea69b28a90a0
elanajaja <elanjaja01@gmail.com>
2024-01-16 07:09:40 -0800

    Write a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,40 +1,55 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at index
- * @h: the head:
- * @idx: the index
- * @n: what to add
- * Return: new nodes
+ * insert_dnodeint_at_index - Add node at nth index
+ *
+ * @h: Head of node
+ *
+ * @idx: index
+ *
+ * @n: struct int
+ *
+ * Return: dlistint_t
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *last, *back;
-	unsigned int r = 0;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_t *current;
+	unsigned int count = 0;
 
-	if (*h == NULL)
-	{
-		return (add_dnodeint_end(h, n));
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (h == NULL || new_node == NULL)
 	{
 		return (NULL);
 	}
-	last = *h;
-	while (last)
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	current = *h;
+
+	if (idx == 0)
 	{
-		if (idx == r)
-		{
-			new->n = n;
-			back = last->prev;
-			new->next = last;
-			new->prev = back;
-			back->next = new;
-			return (*h);
-		}
-		last = last->next;
-		r++;
+		new_node = add_dnodeint(h, n);
+		return (new_node);
 	}
+	while (current)
+	{
+		if (current->next == NULL && count == idx - 1)
+		{
+			new_node = add_dnodeint_end(h, n);
+			return (new_node);
+		}
+		else if ((idx - 1) == count)
+		{
+			new_node->next = current->next;
+			new_node->prev = current;
+			current->next->prev = new_node;
+			current->next = new_node;
+			return (new_node);
+		}
+		count++;
+		current = current->next;
+	}
+	free(new_node);
 	return (NULL);
 }
