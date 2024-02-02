9ab52bee01a8d8473880224efacc73c15d3f460c
elanajaja <elanjaja01@gmail.com>
2024-02-02 13:59:11 -0800

    hash_tables.h 0-hash_table_create.c files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = NULL;
+	unsigned long int i = 0;
+
+	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array =
+		(hash_node_t **)calloc(table->size, sizeof(hash_node_t *));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	while (i < table->size)
+	{
+		table->array[i] = NULL;
+		i++;
+	}
+
+	return (table);
+}
